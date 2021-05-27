e65f22e89f0dcfc883cdb5bf0f7d2926d7b8d366
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-27 12:44:00 -0400

    commit tree e0220ad1cc649fa46dff732f64ea2fbdf9d3b39b

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+export default function SimplerComponent(props) {
+  return (
+    
+    
+    <div onClick={props.handleClick}>
+      I am just happy
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert('I was Clicked')} />
   </div>,
   document.getElementById('root')
 );
